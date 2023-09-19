ed18de2619b6b7cbf2ee15a391aaa647c87a8f9d
mametchi <tinyaqui@gmail.com>
2023-09-19 16:06:03 +0300

    Moving files from a local PyCharm project

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+import random
+from transformers import pipeline
+
+pipe = pipeline("translation", model="Helsinki-NLP/opus-mt-en-fi")
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+   translation_request = request.form["translation_request" ]
+
+   random_number = random.randint(0,100)
+
+   return f'{translation_request}. Ja satunnainen luku: {random_number}'
+
+if __name__ == '__main__':
+   app.run(debug=True)
